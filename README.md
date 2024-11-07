4e647081b35b3be92dd8cbde5c651d884d5f9525
Saif Shaikh <Saif Shaikh>
2024-11-07 13:47:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Employee Management System
+
+I have made a compact yet feature-rich employee management system where admins can create tasks, assign them to users, and monitor their status, including completed, pending, or failed. Users can view assigned tasks, accept or forward them, and update their progress by marking tasks as completed or failed, ensuring streamlined task tracking and accountability.
+
+## 🛠 Skills
+Javascript, React, Context Api, Tailwind css and Local Storage handling...
+## Run Locally
+
+## Features
+- Admin and user roles with distinct privileges
+- Task assignment, forwarding, and status updates
+- Real-time task tracking and filtering (completed, pending, or failed)
+
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/Employee.Dashboard'
+import AdminDashboard from './components/Dashboard/Admin.Dashboard'
+import { getLocalStroage, setLocalStroage } from './utils/LocalStrorage'
+import { AuthContext } from './context/AuthProvider'
+
+const App = () => {
+  const [user, setUser]= useState(null)
+  const[loggedInUserData, setLoggedInUserData]= useState(null)
+  
+  const [userData, setUserData] = useContext(AuthContext)
+  // console.log(authData)
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    if (loggedInUser) {
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    } else {
+      // Only set local storage if there is no logged-in user
+      setLocalStroage()
+    }
+  }, [])
+
+  const handleLogin= (email, password)=>{
+    if(email == 'admin@example.com' && password == '123'){
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}))
+    }else if(userData){
+      const employee = userData.find( (e) => email == e.email && password == e.password )
+      if(employee){
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({role:'employee', data: employee}))
+      }
+    }else{
+      alert("Invalid Credentials")
+    }
+  }
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
+    if (user && user.data) {
+      const firstName = user.data.firstName;
+      const employee = userData.find((e) => e.firstName === firstName);
+      if (employee) {
+        setLoggedInUserData(employee)
+      }
+    }
+  }, [userData])
+
+  return (
+    <>
+      {!user ? <Login handleLogin={handleLogin} />: ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : ( user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null)}
+    </>
+  )
+}
+
+export default App
